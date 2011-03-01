2699727ffeae750277719a2eb22f7614f1dd9ae1
Robert Niederreiter <rnix@squarewave.at>
2011-03-01 15:30:08 +0100

    chmeleon 2.0rc2 compat

--- src/cone/app/browser/layout.py
+++ src/cone/app/browser/layout.py
@@ -67,7 +67,7 @@ class MainMenu(Tile):
             item = dict()
             item['id'] = key
             if empty_title:
-                item['title'] = '&nbsp;'
+                item['title'] = ' '
                 item['description'] = child.metadata.title
             else:
                 item['title'] = child.metadata.title
