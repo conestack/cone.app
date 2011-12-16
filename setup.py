38a8a909bfa155e0e147d81ad6d60ee4afdbb728
Robert Niederreiter <rnix@squarewave.at>
2011-12-16 18:21:08 +0100

    0.9.1pre3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.9.1pre2'
+version = '0.9.1pre3'
 shortdesc = 'Web application stub'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
