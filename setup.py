ac9c866843c48dabead4ceba0ed608730de1cc3e
Robert Niederreiter <office@squarewave.at>
2017-08-28 09:52:04 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_file(name):
         return f.read()
 
 
-version = '1.0a6'
+version = '1.0a7.dev0'
 shortdesc = 'Web application stub'
 longdesc = '\n\n'.join([read_file(name) for name in [
     'README.rst',
