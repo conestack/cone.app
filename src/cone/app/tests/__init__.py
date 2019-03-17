60bbed0a041086767e707f454436a23f895019c1
Robert Niederreiter <office@squarewave.at>
2019-03-17 16:57:04 +0100

    Transform browser __init__ doctests to unittests done

--- src/cone/app/tests/__init__.py
+++ src/cone/app/tests/__init__.py
@@ -2,17 +2,27 @@ import unittest
 
 
 def test_suite():
+    from cone.app.tests import test_testing
+
     from cone.app.tests import test_app
     from cone.app.tests import test_model
     from cone.app.tests import test_security
     from cone.app.tests import test_utils
+    from cone.app.tests import test_workflow
+
+    from cone.app.tests import test_browser
 
     suite = unittest.TestSuite()
+
+    suite.addTest(unittest.findTestCases(test_testing))
 
     suite.addTest(unittest.findTestCases(test_app))
     suite.addTest(unittest.findTestCases(test_model))
     suite.addTest(unittest.findTestCases(test_security))
     suite.addTest(unittest.findTestCases(test_utils))
+    suite.addTest(unittest.findTestCases(test_workflow))
+
+    suite.addTest(unittest.findTestCases(test_browser))
 
     return suite
 
