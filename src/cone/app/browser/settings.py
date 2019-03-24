f166b693c509651b963dab460330099b84a9a093
Robert Niederreiter <office@squarewave.at>
2019-03-24 14:49:56 +0100

    use str.format() in browser settings

--- src/cone/app/browser/settings.py
+++ src/cone/app/browser/settings.py
@@ -27,8 +27,8 @@ def settings_tab_content(model, request):
     except Exception:
         localizer = get_localizer(request)
         error = localizer.translate(_('error', default='Error'))
-        rendered = '<div>%s: %s</div>' % (error, format_traceback())
-    return Response('<div class="%s">%s</div>' % (model.name, rendered))
+        rendered = '<div>{}: {}</div>'.format(error, format_traceback())
+    return Response('<div class="{}">{}</div>'.format(model.name, rendered))
 
 
 @tile(name='content',
@@ -43,9 +43,10 @@ class AppSettings(Tile):
         for val in self.model.values():
             ret.append({
                 'title': val.metadata.title,
-                'target': make_url(self.request,
-                                   node=val,
-                                   resource='settings_tab_content'),
+                'target': make_url(
+                    self.request,
+                    node=val,
+                    resource='settings_tab_content'),
             })
         return ret
 
@@ -57,11 +58,11 @@ class SettingsBehavior(Behavior):
     @plumb
     def prepare(_next, self):
         _next(self)
-        selector = '#form-%s' % '-'.join(self.form.path)
+        selector = '#form-{}'.format('-'.join(self.form.path))
         ajax_form_fiddle(self.request, selector, 'replace')
 
     @default
     def next(self, request):
         url = make_url(request.request, node=self.model)
-        selector = '.%s' % self.model.name
+        selector = '.{}'.format(self.model.name)
         return [AjaxAction(url, 'content', 'inner', selector)]
