d5a929d87c58ce54b850e91bc273312e3d310352
Robert Niederreiter <office@squarewave.at>
2019-03-25 16:11:58 +0100

    Another attempt to integrate coveralls

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-./$1/bin/coverage run --source src/cone/app src/cone/app/tests/__init__.py
+./$1/bin/coverage run --source src/cone/app -m cone.app.tests.__init__
 ./$1/bin/coverage report
 ./$1/bin/coverage html
