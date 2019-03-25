d0ac7f4d5e2a1b1821f1665907a199ae694e293b
Robert Niederreiter <office@squarewave.at>
2019-03-25 15:13:46 +0100

    First attempt to integrate coveralls

--- src/cone/app/compat.py
+++ src/cone/app/compat.py
@@ -1,9 +1,9 @@
-try:
+try:  # pragma: no cover
     from urllib2 import quote
     from urllib2 import unquote
     import ConfigParser as configparser
     import urlparse
-except ImportError:
+except ImportError:  # pragma: no cover
     from urllib.parse import quote
     from urllib.parse import unquote
     import configparser
@@ -13,9 +13,7 @@ import types
 
 
 IS_PY2 = sys.version_info[0] < 3
-BYTES_TYPE = str if IS_PY2 else bytes
 STR_TYPE = basestring if IS_PY2 else str
 UNICODE_TYPE = unicode if IS_PY2 else str
-LONG_TYPE = long if IS_PY2 else int
 ITER_TYPES = (types.ListType, types.TupleType) if IS_PY2 else (list, tuple)
 NUMBER_TYPES = (types.IntType, types.FloatType) if IS_PY2 else (int, float)
